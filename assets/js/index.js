4937a85a8531e291a6c76f0effc5dd108b3d0de8
Ahmad Ateya <ahmad.m.ateya@gmail.com>
2021-12-25 00:43:02 +0200

    WIP

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,6 +4,10 @@ if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').match
 window.onload = (event) => {
   var elements = document.getElementsByClassName('level-bar-inner')
   for (let i = 0; i < elements.length; i++) {
+    if (elements[i].dataset.level === 'no-level') {
+        elements[i].parentElement.classList.remove('level-bar ');
+        continue;
+    }
     elements[i].style.width = elements[i].dataset.level*50 + 'px'
   }
 }
